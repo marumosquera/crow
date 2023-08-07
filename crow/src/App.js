63372bed974a88441f986237cc1a22f7e92f1146
marumosquera <mosqueramariaelisa1998@gmail.com>
2023-08-07 15:03:34 -0300

    feat: about page + roadmap + tutorial

---
+++ crow/src/App.js
@@ -0,0 +1,27 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { ErrorPage } from "./views/ErrorPage";
+import { Home } from "./views/Home";
+import { Footer } from "./components/Footer";
+import { Tutorial } from "./views/Tutorial";
+import { About } from "./views/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+        <Footer/>
+    </div>
+  </BrowserRouter>
+  );
+}
+
+export default App;
